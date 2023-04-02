7fc31e3ec6895b11f9cdc50575a85072a6826403
ricksonrezende <ricksonrezende1@gmail.com>
2023-04-01 21:54:32 -0300

    Implementado método para abrir o arquivo de texto

--- Program.cs
+++ Program.cs
@@ -31,7 +31,19 @@ namespace EditorTexto
 
         static void Abrir()
         {
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+            string path = Console.ReadLine();
 
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
         }
 
         static void Editar()
